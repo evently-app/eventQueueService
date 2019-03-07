cf8ce42c7c3c93135f5bc8070a1e7dcbbc537d6a
leaspark <lea.sparkman@yale.edu>
2019-03-06 23:30:45 -0500

    fixed ticketmaster - added coordinates

--- sources/ticketmaster.js
+++ sources/ticketmaster.js
@@ -4,66 +4,43 @@ var request = require('request');
 var SourceConstructor = require('../classes/sourceConstructor');
 
 var ticketmaster = new SourceConstructor({
-		url : "https://app.ticketmaster.com/discovery/v2/events.json?", 
-		apiTerm: "apikey", 
-	    token: process.env.TICKETMASTER_API_KEY, 
-	    paramMap: {"city": "city"}, 
-	    apiName: 'Ticketmaster',
+	url : "https://app.ticketmaster.com/discovery/v2/events.json?", 
+	apiTerm: "apikey", 
+    token: process.env.TICKETMASTER_API_KEY, 
+    paramMap: {"city": "city"}, 
+    apiName: 'Ticketmaster',
 
-	    formatEvents: function(res) {
-	    	var formattedEvents = []
-	    	var responseSize = Object.keys(res["_embedded"]["events"]).length;
-	    	for(var i = 0; i<responseSize; i++){
+    formatEvents: function(res) {
+    	var formattedEvents = []
+    	var responseSize = Object.keys(res["_embedded"]["events"]).length;
+    	for(var i = 0; i<responseSize; i++){
 
-	    		try{
+    		try{
+    			var locationInfo = {};
+    			locationInfo = zipcodes.lookup(res["_embedded"]["events"][i]["_embedded"]["venues"][0]["postalCode"]);
 
-	    			var venueRequest = "https://app.ticketmaster.com" +(res["_embedded"]["events"][i]["_links"]["venues"][0]["href"]) + "&" +this.apiTerm + "=" + this.token;
-	    			var location = 0; 
-	    			var locationInfo = {};
-	    			var getVenue = new Promise(function(resolve, reject) {
-	    				request(venueRequest,
-	    					function(error, response, body) {
-	    						if(!error && response.statusCode == 200){
-	    							response = JSON.parse(body);
-	    							resolve(response);
+			    var event = {
+			    	eventName: res["_embedded"]["events"][i]["name"],
+			    	startTime: res["_embedded"]["events"][i]["dates"]["dateTime"],
+				    endTime: null,
+				    ticketUrl: res["_embedded"]["events"][i]["url"],
+				    id: res["_embedded"]["events"][i]["id"],
+				    tags: ["ticketmaster"],
+				    imageUrl: res["_embedded"]["events"][i]["images"],
+				    latitude: locationInfo.latitude,
+				    longitude: locationInfo.longitude
+			    }
 
-	    						} else {
-	    							console.log(response.statusCode + response.body);
-	    						}
-	    					})
-	    			})
-
-	    			getVenue.then(function(result){
-	    				locationInfo = zipcodes.lookup(result["postalCode"]);
-	    				console.log("THIS IS HERE! ", locationInfo)
-	    			})
-
-				    var event = {
-				    	eventName: res["_embedded"]["events"][i]["name"],
-				    	startTime: res["_embedded"]["events"][i]["dates"]["dateTime"],
-					    endTime: null,
-					    ticketUrl: res["_embedded"]["events"][i]["url"],
-					    id: res["_embedded"]["events"][i]["id"],
-					    tags: ["ticketmaster"],
-					    imageUrl: res["_embedded"]["events"][i]["images"],
-					    latitude: locationInfo.latitude,
-					    longitude: locationInfo.longitude
-				    }
-
-				    // formattedEvents.push(new EventObject(event));
-				    formattedEvents.push(event);
-				}
-				catch(err){
-		    		console.log("An event from ticketmaster does not have all required fields.\n"+err.message);
-				}
-			    
+			    // formattedEvents.push(new EventObject(event));
+			    formattedEvents.push(event);
 			}
-			return formattedEvents;
-	    }
+			catch(err){
+	    		console.log("An event from ticketmaster does not have all required fields.\n"+err.message);
+			}
+		}
+		return formattedEvents;
     }
-
-
-    );
+  });
 
 ticketmaster.getRequestUrl = function(req) {
 	//do stuff 
