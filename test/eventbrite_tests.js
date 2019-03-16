8011f6a994c2023db63671c66181bd4abe7ed4ca
David Y Liu <davidyliuliu@gmail.com>
2019-03-16 16:03:06 -0700

    Added few more basic eventbrite tests

--- test/eventbrite_tests.js
+++ test/eventbrite_tests.js
@@ -4,17 +4,33 @@ chai.use(chaiHttp);
 
 var expect = chai.expect;
 
+
+// *****ASYNC CURRENTLY NOT WORKING, UNSURE WHY
+
 describe('eventbriteSimpleAPICall', function() {
-  describe('#callSuccess', function() {
-	it('success, as expected', function(done) { 
-	  chai.request('https://www.eventbriteapi.com/v3/events')
+  this.timeout(5000);
+
+  describe('#differentParams', function() {
+	it('city + radius', async function() { 
+	  const res = await chai.request('https://www.eventbriteapi.com/v3/events')
 	  .get('/search')
-	  .query({'location.address': 'newhaven', 'location.within': '10km', 'token': process.env.EVENTBRITE_API_KEY})
-	  .end(function(err, res) {
-	     expect(err).to.be.null;
-	     expect(res).to.have.status(200);
-	     done();
-	  });
+	  .query({'location.address': 'newhaven', 'location.within': '10km', 'token': process.env.EVENTBRITE_API_KEY});
+	  expect(res).to.have.status(200);
+	});
+
+	it('longitude + latitude + radius', async function() { 
+	  const res = await chai.request('https://www.eventbriteapi.com/v3/events')
+	  .get('/search')
+	  .query({'location.longitude': '41.310726', 'location.latitude': '-72.929916',
+	  	'location.within': '3km', 'token': process.env.EVENTBRITE_API_KEY});
+	  expect(res).to.have.status(200);
+	});
+
+	it('city + expand venue', async function() { 
+	  const res = await chai.request('https://www.eventbriteapi.com/v3/events')
+	  .get('/search')
+	  .query({'location.address': 'newhaven', "expand":"venue", 'token': process.env.EVENTBRITE_API_KEY});
+	  expect(res).to.have.status(200);
 	});
   });
 });
