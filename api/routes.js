51ab2a3d86d85258cd4c5b5281a4615b52ac1aaa
Sahib Grover <sahibsinghgrover@hotmail.com>
2019-04-12 10:03:25 -0400

    Meetups scraping added to cron job. Fully functional and connected to firebase. Deleted sources, all traces of "grabbing" functionality which isn't required anymore. Deleted classes which aren't being used like sourceconstructor and eventobject.

--- api/routes.js
+++ api/routes.js
@@ -5,8 +5,6 @@ var controller = require('./controller');
 module.exports = function(app) {
    app.route('/about')
        .get(controller.about);
-   app.route('/grab_events/:latitude/:longitude/:radius')
-       .get(controller.grabEvents);
     app.route('/scrape/')
        .get(controller.scrape);
     app.route('/ping_events_queue')
